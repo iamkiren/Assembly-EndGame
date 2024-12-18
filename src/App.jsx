885335018addb773787ccbd7c75794bcc1e1e22e
Kiren Raj <iamkiren@gmail.com>
2024-12-18 19:28:02 +0530

    added some basic ui and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,55 @@
-import "./App.css";
+import { useState } from "react";
+import { languages } from "./languages";
 
-function App() {
-  return <h1>Hello World</h1>;
+/**
+ * Goal: Build out the main parts of our app
+ *
+ * Challenge:
+ * Display the keyboard ⌨️. Use <button>s for each letter
+ * since it'll need to be clickable and tab-accessible.
+ */
+
+export default function AssemblyEndgame() {
+  const [currentWord, setCurrentWord] = useState("react");
+
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+  const keyboardElements = alphabet.split("").map((letter) => {
+    return <button key={letter}>{letter.toUpperCase()}</button>;
+  });
+
+  const languageElements = languages.map((lang) => {
+    const styles = {
+      backgroundColor: lang.backgroundColor,
+      color: lang.color,
+    };
+    return (
+      <span className="chip" style={styles} key={lang.name}>
+        {lang.name}
+      </span>
+    );
+  });
+
+  const letterElements = currentWord
+    .split("")
+    .map((letter, index) => <span key={index}>{letter.toUpperCase()}</span>);
+
+  return (
+    <main>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>
+          Guess the word within 8 attempts to keep the programming world safe
+          from Assembly!
+        </p>
+      </header>
+      <section className="game-status">
+        <h2>You win!</h2>
+        <p>Well done! 🎉</p>
+      </section>
+      <section className="language-chips">{languageElements}</section>
+      <section className="word">{letterElements}</section>
+      <section className="keyboard">{keyboardElements}</section>
+    </main>
+  );
 }
-
-export default App;
